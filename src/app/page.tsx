9ba51de5b4c5fd0c961745f0ac799615d1ffc9de
RobertoDev3 <roberto.dev3@hotmail.com>
2025-03-25 12:38:00 -0300

    add new SVG icons for navigation and actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,28 @@
-import { IconFavorite } from "@/assets/icons/svg/icon-favorite";
-import { IconFavoriteFill } from "@/assets/icons/svg/icon-favorite-fill";
-import { IconFavoriteV2 } from "@/assets/icons/svg/icon-favorite-v2";
-import { IconFavoriteV2Fill } from "@/assets/icons/svg/icon-favorite-v2-fill";
-import { IconFavoriteV3 } from "@/assets/icons/svg/icon-favorite-v3";
-import { IconFavoriteV3Fill } from "@/assets/icons/svg/icon-favorite-v3-fill";
-import { IconPokedex } from "@/assets/icons/svg/icon-pokedex";
-import { IconPokedexFill } from "@/assets/icons/svg/icon-pokedex-fill";
-import { IconProfile } from "@/assets/icons/svg/icon-profile";
-import { IconProfileFill } from "@/assets/icons/svg/icon-profile-fill";
-import { IconRegion } from "@/assets/icons/svg/icon-region";
-import { IconRegionFill } from "@/assets/icons/svg/icon-region-fill";
+import { IconArrowLeft } from "@/assets/icons/svg/icon-arrow-left";
+import { IconChevronLeft } from "@/assets/icons/svg/icon-chevron-left";
+import { IconEyeClosed } from "@/assets/icons/svg/icon-eye-closed";
+import { IconEyeOpen } from "@/assets/icons/svg/icon-eye-open";
+import { IconSearch } from "@/assets/icons/svg/icon-search";
+import { IconTrash } from "@/assets/icons/svg/icon-trash";
 
 export default function Home() {
   return (
-    <main className="m-12 flex flex-col gap-4 bg-accent-foreground">
+    <main className="m-12 flex flex-col gap-4 bg-red-300">
       <div className="flex gap-4">
-        <IconPokedexFill className="size-10" />
-        <IconPokedex className="size-10" />
+        <IconEyeOpen className="size-10" />
+        <IconEyeClosed className="size-10" />
       </div>
       <div className="flex gap-4">
-        <IconRegionFill className="size-10" />
-        <IconRegion className="size-10" />
+        <IconChevronLeft className="size-10" />
       </div>
       <div className="flex gap-4">
-        <IconFavoriteFill className="size-10" />
-        <IconFavorite className="size-10" />
+        <IconArrowLeft className="size-10" />
       </div>
       <div className="flex gap-4">
-        <IconProfileFill className="size-10" />
-        <IconProfile className="size-10" />
+        <IconSearch className="size-10" />
       </div>
       <div className="flex gap-4">
-        <IconFavoriteV2Fill className="size-10" />
-        <IconFavoriteV2 className="size-10" />
-      </div>
-      <div className="flex gap-4">
-        <IconFavoriteV3Fill className="size-10" />
-        <IconFavoriteV3 className="size-10" />
+        <IconTrash className="size-10" />
       </div>
     </main>
   );
