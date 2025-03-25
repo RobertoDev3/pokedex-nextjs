eaa9e93293085a476901ed3a9fb8ccfb0e93f42d
RobertoDev3 <roberto.dev3@hotmail.com>
2025-03-25 10:55:33 -0300

    initialize Next.js project with Tailwind CSS and essential configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "tailwindcss/no-custom-classname": "off",
+      "react/jsx-props-no-spreading": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "react/prop-types": "off",
+      "import/no-duplicates": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".tsx"] }
+      ],
+      "import/prefer-default-export": "off",
+      "@typescript-eslint/camelcase": "off",
+      "import/no-anonymous-default-export": [
+        "warn",
+        { allowObject: true, allowArrowFunction: true }
+      ],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+    },
+  }),
+];
+
+export default eslintConfig;
