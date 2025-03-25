59bbfeb5df65c6355e864dc71584a7dd107a0eab
RobertoDev3 <roberto.dev3@hotmail.com>
2025-03-25 12:24:58 -0300

    add new SVG icons for favorites, profile, region, and pokedex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,43 @@
-import { Button } from "@/components/ui/button";
+import { IconFavorite } from "@/assets/icons/svg/icon-favorite";
+import { IconFavoriteFill } from "@/assets/icons/svg/icon-favorite-fill";
+import { IconFavoriteV2 } from "@/assets/icons/svg/icon-favorite-v2";
+import { IconFavoriteV2Fill } from "@/assets/icons/svg/icon-favorite-v2-fill";
+import { IconFavoriteV3 } from "@/assets/icons/svg/icon-favorite-v3";
+import { IconFavoriteV3Fill } from "@/assets/icons/svg/icon-favorite-v3-fill";
+import { IconPokedex } from "@/assets/icons/svg/icon-pokedex";
+import { IconPokedexFill } from "@/assets/icons/svg/icon-pokedex-fill";
+import { IconProfile } from "@/assets/icons/svg/icon-profile";
+import { IconProfileFill } from "@/assets/icons/svg/icon-profile-fill";
+import { IconRegion } from "@/assets/icons/svg/icon-region";
+import { IconRegionFill } from "@/assets/icons/svg/icon-region-fill";
 
 export default function Home() {
   return (
-    <main className="m-12 flex flex-col gap-4 w-xs">
-      <Button variant="water">Água</Button>
-      <Button variant="dragon">Dragão</Button>
-      <Button variant="eletric">Elétrico</Button>
-      <Button variant="fairy">Fada</Button>
-      <Button variant="ghost">Fantasma</Button>
-      <Button variant="fire">Fogo</Button>
-      <Button variant="ice">Gelo</Button>
-      <Button variant="grass">Grama</Button>
-      <Button variant="bug">Inseto</Button>
-      <Button variant="fighting">Lutador</Button>
-      <Button variant="normal">Normal</Button>
-      <Button variant="dark">Noturno</Button>
-      <Button variant="steel">Metal</Button>
-      <Button variant="rock">Pedra</Button>
-      <Button variant="psychic">Psíquico</Button>
-      <Button variant="ground">Terrestre</Button>
-      <Button variant="poison">Venenoso</Button>
-      <Button variant="flying">Voador</Button>
+    <main className="m-12 flex flex-col gap-4 bg-accent-foreground">
+      <div className="flex gap-4">
+        <IconPokedexFill className="size-10" />
+        <IconPokedex className="size-10" />
+      </div>
+      <div className="flex gap-4">
+        <IconRegionFill className="size-10" />
+        <IconRegion className="size-10" />
+      </div>
+      <div className="flex gap-4">
+        <IconFavoriteFill className="size-10" />
+        <IconFavorite className="size-10" />
+      </div>
+      <div className="flex gap-4">
+        <IconProfileFill className="size-10" />
+        <IconProfile className="size-10" />
+      </div>
+      <div className="flex gap-4">
+        <IconFavoriteV2Fill className="size-10" />
+        <IconFavoriteV2 className="size-10" />
+      </div>
+      <div className="flex gap-4">
+        <IconFavoriteV3Fill className="size-10" />
+        <IconFavoriteV3 className="size-10" />
+      </div>
     </main>
   );
 }
